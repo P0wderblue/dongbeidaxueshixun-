76e80489c9c4d23a17c010327e42d63eb68aae32
lyt <1115630547@qq.com>
2022-06-12 01:24:00 +0800

    教学

--- projectManagement/src/main/java/com/projectManagement/ProjectServiceApplication.java
+++ projectManagement/src/main/java/com/projectManagement/ProjectServiceApplication.java
@@ -11,5 +11,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ProjectServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectServiceApplication.class,args);
+        //教上传
     }
 }
